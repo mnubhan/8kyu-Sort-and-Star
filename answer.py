e5ff9f064c3fc341c87ff289b4a32b0647e2133d
mnubhan <76425959+mnubhan@users.noreply.github.com>
2023-10-05 00:45:03 +0100

    Update answer.py

--- answer.py
+++ answer.py
@@ -1,3 +1,9 @@
 def two_sort(array):
   array.sort()
   return "***".join([x for x in array[0]])
+
+def two_sort(arr):
+    return '***'.join(sorted(arr)[0])
+
+def two_sort(lst):
+    return '***'.join(min(lst))
